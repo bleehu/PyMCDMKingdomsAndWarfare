f1a7751018839dc11ccdc418dcce0a041793bc9b
bleehu <michael.serious.hedges@gmail.com>
2023-05-30 13:26:18 -0700

    version minot bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="KingdomsAndWarfare",
-    version="0.2.0",
+    version="0.2.1",
     description="Kingdoms, Units, Unit Traits all for MCDM's excellent expansion for D&D",
     packages=["KingdomsAndWarfare.Units", "KingdomsAndWarfare.Traits", "KingdomsAndWarfare.Kingdoms"],
     package_dir={"KingdomsAndWarfare":"KingdomsAndWarfare"},
